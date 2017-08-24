d221db35b955c929a8d95afaef2a3820358ccd1e
Fernando Soares <dotfernando@gmail.com>
2017-08-23 22:09:43 -0300

    subindo

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var tasks = ["Item 1", "Item 2", "Item 3", "Item 4"]
+
+for item in tasks {
+    print(item)
+}
+
+print("Temos", tasks.count, "Tarefas")
